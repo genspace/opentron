7b7eeac5ab6083a4a22b8e18be7e91ecdf096126
Gordon Fleetwood <gfleetwood@protonmail.com>
2020-06-06 00:04:48 -0400

    Clean up

--- scripts/mixer.py
+++ scripts/mixer.py
@@ -7,11 +7,10 @@ from opentrons import labware, instruments
 metadata = {
     'protocolName': 'My Protocol',
     'author': 'WTMoose <wtmoose@thingconnect.cc>',
-    'description': Oh, Fry! I love the way you mix three things.',
+    'description': 'Oh, Fry! I love the way you mix three things.',
 }
 
 # labware
-
 
 # In the original example
 tiprack = labware.load('opentrons-tiprack-300ul', '1')
@@ -25,7 +24,6 @@ trough = labware.load('usascientific_12_reservoir_22ml', '2')
 # Try this plate which I found in the labware library. Don’t forget to comment out the above. Changing it to 
 plate = labware.load('corning_24_wellplate_3.4ml_flat', '3')
 
-
 # pipettes
 pipette = instruments.P300_Single(mount='right', tip_racks=[tiprack])
 
@@ -33,8 +31,6 @@ pipette = instruments.P300_Single(mount='right', tip_racks=[tiprack])
 
 # This squirts the shmoo into the bottom of the well at plate-well A1
 pipette.transfer(200, trough.wells('A1'), plate.wells('A1'))
-
-
 
 #pipette.transfer(200, trough.wells('A2'), plate.wells('A1'))
 #pipette.transfer(200, trough.wells('A3'), plate.wells('A1'))
--- scripts/test.py
+++ scripts/test.py
@@ -1,32 +1,4 @@
 # imports
-from opentrons import labware, instruments
-
-# metadata
-metadata = {
-    'protocolName': 'My Protocol',
-    'author': 'WTMoose <wtmoose@thingconnect.cc>',
-    'description': 'Simple protocol, work, dammit!',
-}
-
-# labware
-#plate = labware.load('96-flat', '2')
-
-# Try this plate which I found in the labware library. Don’t forget to comment out the above.
-plate = labware.load('corning_24_wellplate_3.4ml_flat', '3')
-
-tiprack = labware.load('opentrons-tiprack-300ul', '1')
-
-# pipettes
-pipette = instruments.P300_Single(mount='right', tip_racks=[tiprack])
-
-# commands
-pipette.transfer(100, plate.wells('A1'), plate.wells('B2')) 
-```
-
-Test python script which you can paste into the Jupyter notebook.
-
-```
-# imports
 from opentrons import robot, labware, instruments
 
 robot.connect()
@@ -42,7 +14,7 @@ metadata = {
 plate = labware.load('96-flat', '2')
 
 # Try this plate which I found in the labware library. Don’t forget to comment out the above.
-#plate = labware.load('corning_24_wellplate_3.4ml_flat', '2')
+plate = labware.load('corning_24_wellplate_3.4ml_flat', '3')
 
 tiprack = labware.load('opentrons-tiprack-300ul', '1')
 
